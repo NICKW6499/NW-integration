a8861fc4ea3e8b6cf3144789fbf67fb7a7085372
Nicholis Wright <ngwright3574@eagle.fgcu.edu>
2018-09-06 09:57:49 -0400

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,7 @@
+//Nicholis Wright
+// a creepy program to track your mother when she gets lost
+public class main {
+public static void main(String[] args) {
+	System.out.println("Hello Peasant");
+	}
+}
